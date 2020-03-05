a7c958008d9c586e397ee1cf3ad695907309808a
Ty Raino <mstymetris@yahoo.com>
2020-03-05 17:53:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown(second)
+def countdown(seconds)
   while second > 0 
-  puts "#{second} SECOND(S)!"
-  second += 1
+  puts "#{seconds} SECOND(S)!"
+  seconds -= 1
 end
 return "HAPPY NEW YEAR!"
 end
