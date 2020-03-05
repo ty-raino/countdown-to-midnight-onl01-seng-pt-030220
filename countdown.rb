4f2567bbaa5a101c954b76aca57977b5e68da1f6
Ty Raino <mstymetris@yahoo.com>
2020-03-05 17:55:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(seconds)
 end
 return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+  puts "#{seconds} SECOND(S)!"
+  sleep 1
+  seconds -= 1
+end
+return "HAPPY NEW YEAR"
+end
